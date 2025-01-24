921e32dcae91e2219ad31123f80ae98b93ed42bb
Lorenzo Delgado <lnsdev@proton.me>
2025-01-24 12:40:40 +0100

    Apply devkitpro/buildscripts newlib-4.4.0.20231231.patch
    Apply DevkitPro's devkita64 patch file: https://github.com/devkitPro/buildscripts/blob/1b09b3a82495da820885f8102726c47247f5b05a/dka64/patches/newlib-4.4.0.20231231.patch

---
+++ libgloss/rs6000/crtmain.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+struct __argv {
+	int argvMagic;		//!< argv magic number, set to 0x5f617267 ('_arg') if valid 
+	char *commandLine;	//!< base address of command line, set of null terminated strings
+	int length;//!< total length of command line
+	int argc;
+	char **argv;
+};
+
+extern struct __argv *__system_argv;
+void __init();
+void SYS_PreMain();
+int main(int argc, char **argv);
+
+void __crtmain() {
+	SYS_PreMain();
+	exit ( main(__system_argv->argc,__system_argv->argv) );
+}
